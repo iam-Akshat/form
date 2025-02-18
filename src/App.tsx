2e7b945a6a136f33f672785e383718f4e53dab0b
Akshat Sethi <akshat@nova-benefits.com>
2025-02-18 17:54:54 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <div className="font-bold text-gray-950 text-9xl">
+        hi
+       </div>
+       <Button variant={"outline"}>Hiiii</Button>
+    </>
+  )
+}
+
+export default App
